8bef47777e350241a29a794f3dfd82ff4d8bb0be
MatusJ <matusjelinek@gmail.com>
2018-01-12 18:44:27 +0100

    Edit - all works

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", TopicController, :index
-    #get "/topic", TopicController, :index
+    get "/topics", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "topics/:id/edit", TopicController, :edit
