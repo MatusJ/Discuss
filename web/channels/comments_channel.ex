ec7a795c16166209ad67e121b137b6314224215d
MatusJ <matusjelinek@gmail.com>
2018-01-17 11:34:22 +0100

    Got associated topics with comments

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,7 +1,7 @@
 defmodule Discuss.CommentsChannel do
     use Discuss.Web, :channel 
 
-    alias Discuss.Topic
+    alias Discuss.{Topic, Comment}
 
     # called when first JS client is attempting to join channel
     # onec during application lifecycle
@@ -9,15 +9,22 @@ defmodule Discuss.CommentsChannel do
     def join("comments:" <> topic_id, _params, socket) do
         # IO.puts "++++++++++"
         # IO.inspect name
+        # IO.inspect topic_id
         # name something like comments:24
         # replace name with pattern matching
         # we want integer not string
         topic_id = String.to_integer(topic_id)
         # we cannot sand it as JSON
-        topic = Repo.get(Topic, topic_id)
+        topic = Topic
+            |> Repo.get(topic_id)
+            # load associations
+            |> Repo.preload(:comments)
 
         # {:ok, %{topic: topic}, socket}
-        {:ok, %{}, socket}
+        # can assign data to socket
+        # socket behave similar as conn previously
+        # {:ok, %{}, socket}
+        {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
     end
 
     # follow up communication
@@ -27,8 +34,18 @@ defmodule Discuss.CommentsChannel do
         # IO.inspect message
         # message has content from JS
 
+        topic = socket.assigns.topic
 
-        # paasing error when users gonna get data that he is not supposed to
-        {:reply, :ok, socket}
+        changeset = topic
+            |> build_assoc(:comments)
+            |> Comment.changeset(%{content: content})
+
+        case Repo.insert(changeset) do
+            {:ok, comment} ->
+                # paasing error -> when users gonna get data that he is not supposed to
+                {:reply, :ok, socket}
+            {:error, _reason} ->
+                {:reply, {:error, %{errors: changeset}}, socket}
+        end
     end
 end
--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,6 +1,10 @@
 defmodule Discuss.Comment do
     use Discuss.Web, :model
 
+    # whenever poison looks on this model
+    # just look at content
+    @derive {Poison.Encoder, only: [:content]}
+
     schema "comments" do
         field :content, :string
         belongs_to :user, Discuss.User
